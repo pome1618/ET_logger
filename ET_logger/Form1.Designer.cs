1210ba764fd26b7b885a4a193c881f4ef6ba6c56
pome1618 <pome1618@gmail.com>
2013-08-13 22:51:13 +0900

    Ver 1.00

---
+++ ET_logger/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace ET_logger
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
+			this.button5 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(107, 217);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Run";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(229, 217);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "Stop";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("ＭＳ 明朝", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.label1.Location = new System.Drawing.Point(141, 84);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(234, 97);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "0000";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+            "time",
+            "light",
+            "gyro",
+            "motor_R",
+            "motor_L"});
+			this.comboBox1.Location = new System.Drawing.Point(321, 27);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 20);
+			this.comboBox1.TabIndex = 4;
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(61, 27);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(121, 20);
+			this.comboBox2.TabIndex = 5;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(210, 27);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 6;
+			this.button3.Text = "connect";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(448, 25);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 7;
+			this.button4.Text = "set";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(392, 217);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(75, 23);
+			this.button5.TabIndex = 8;
+			this.button5.Text = "Start";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(566, 261);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "ET logger";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button5;
+	}
+}
+
---
+++ ET_logger/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace ET_logger
+{
+	public partial class Form1 : Form
+	{
+		//ポート関係の宣言
+		SerialPort myPort;
+		static string PortName;
+		int BaudRate;
+		Parity Parity;
+		int DataBits;
+		StopBits StopBits;
+
+		//表示する値のコマンド
+		int cmd;
+
+		//読み込むbufferr
+		string buf = "";
+		string buf2 = "";
+		int find = 0;
+
+		//csv用のファイル
+		System.IO.StreamWriter fs;
+
+		//各変数の宣言
+		int time = 0, light = 0, gyro = 0, motor_R = 0, motor_L = 0;
+
+		//Thread
+		System.Threading.Thread Datalogging_Thread;
+		delegate void MyDelegate(string target);
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			//ポートの設定
+			BaudRate = 38400;
+			Parity = Parity.None;
+			DataBits = 8;
+			StopBits = StopBits.One;
+
+			//接続可能なCOMポートを検出、コンボボックスに入れる
+			string[] portNames = SerialPort.GetPortNames();
+
+			// 取得したシリアル・ポート名をコンボボックスにセット
+			comboBox2.Items.Clear();
+			foreach (string port in portNames)
+			{
+				this.comboBox2.Items.Add(port);
+			}
+
+			// コンボボックスのデフォルト選択をセット
+			this.comboBox2.SelectedIndex = 0;
+			this.comboBox1.SelectedIndex = 0;
+
+			//ファイルを作成
+			string FileName = System.DateTime.Now.ToString();	//時間を取得
+			FileName = FileName.Replace('/', '_');				// 2013/07/13を2013_07_13という形に整形
+			FileName = FileName.Replace(":", "_");				//18:23:30を18_23_30という形に整形
+			FileName = FileName.Replace(" ", "_");				//␣を＿に置き換え
+			FileName += ".csv";
+			//MyDocumentsにJ2_logがあったらそこに保存
+			if (System.IO.Directory.Exists(System.Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\J2_log"))
+			{
+				fs = new System.IO.StreamWriter(System.Environment.GetFolderPath(Environment.SpecialFolder.Personal) + "\\J2_log\\"+FileName);
+			}
+			else
+			{
+				fs = new System.IO.StreamWriter(FileName);
+			}
+			MessageBox.Show(FileName, "ファイル作成完了");
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			//コンボボックスからCOMポート名を取得
+			//取得したCOMポート名でポートを開く
+			PortName = comboBox2.Text;
+			if (myPort == null && PortName.Length >= 4 && PortName.Length != 0)
+			{
+				//ポートがnullでポート名が大丈夫そうならポートを開く
+				myPort = new SerialPort(PortName, BaudRate, Parity, DataBits, StopBits);
+				try
+				{
+					myPort.Open();
+				}
+				catch
+				{
+					MessageBox.Show("error", "error");
+				}
+			}
+			if (myPort != null && myPort.IsOpen)
+			{
+				//接続に成功したらメッセージを出す
+				MessageBox.Show("接続成功");
+				//接続に成功したらコンボボックスをロックする
+				comboBox2.Enabled = false;
+			}
+			else
+			{
+				MessageBox.Show("error", "error");
+			}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//'R'を送信する
+			if (myPort != null && myPort.IsOpen)
+			{
+				myPort.Write("R");
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			//'S'を送信する
+			if (myPort != null && myPort.IsOpen)
+			{
+				myPort.Write("S");
+				myPort.Close();
+			}
+		}
+		private void button4_Click(object sender, EventArgs e)
+		{
+			//値(id)を取得
+			switch (comboBox1.SelectedIndex)
+			{
+				case 0: cmd = 0;
+					break;
+				case 1: cmd = 1;
+					break;
+				case 2: cmd = 2;
+					break;
+				case 3: cmd = 3;
+					break;
+				case 4: cmd = 4;
+					break;
+				default: MessageBox.Show("error", "error");
+					break;
+			}
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			//フォームが閉じるときにポートも閉じる
+			if (myPort != null && myPort.IsOpen)
+			{
+				myPort.Close();
+			}
+
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+			button5.Enabled = false;
+			this.Datalogging_Thread =
+			new Thread(new ThreadStart(this.datalogging));
+			this.Datalogging_Thread.Start();
+			button5.Enabled = true;
+
+		}
+
+		public void datalogging()
+		{
+			if (myPort != null && myPort.IsOpen)
+			{
+				//無限ループ
+				while (true)
+				{
+					if (myPort.IsOpen == false)
+					{
+						break;
+					}
+					//bufferの取得
+					try
+					{
+						buf = myPort.ReadLine();
+					}
+					catch
+					{
+						MessageBox.Show("error", "error");
+					}
+					//NULL文字を消す
+					buf = buf.TrimStart('\0');
+
+					//誤作動予防
+					if (buf.Length > 30 || buf.Length < 5)
+					{
+						continue;
+					}
+					if (buf.Length > 30 || buf.Length < 5)
+					{
+						continue;
+					}
+
+					//CSVへの書き込み
+					fs.WriteLine(buf);
+
+					//各変数の取得
+
+					//timeの値取得
+					find = buf.IndexOf(',');
+					if (find <= 0)
+					{
+						continue;
+					}
+					buf2 = buf.Substring(0, find);
+					if (int.TryParse(buf2, out time) == false)
+					{
+						continue;
+					}
+					if (cmd == 0)
+					{
+						label1.Invoke(new MyDelegate(SetText), new object[] { Convert.ToString(time) });
+					}
+
+					//lightの値取得
+					buf = buf.Remove(0, find + 1);
+					find = buf.IndexOf(',');
+					if (find <= 0)
+					{
+						continue;
+					}
+					buf2 = buf.Substring(0, find);
+					if (int.TryParse(buf2, out light) == false)
+					{
+						continue;
+					}
+					if (cmd == 1)
+					{
+						label1.Invoke(new MyDelegate(SetText), new object[] { Convert.ToString(light) });
+					}
+
+					//gyroの値取得
+					buf = buf.Remove(0, find + 1);
+					find = buf.IndexOf(',');
+					if (find <= 0)
+					{
+						continue;
+					}
+					buf2 = buf.Substring(0, find);
+					if (int.TryParse(buf2, out gyro) == false)
+					{
+						continue;
+					}
+					gyro = Convert.ToInt32(buf2);
+					if (cmd == 2)
+					{
+						label1.Invoke(new MyDelegate(SetText), new object[] { Convert.ToString(gyro) });
+					}
+
+					//moter_Rの値取得
+					buf = buf.Remove(0, find + 1);
+					find = buf.IndexOf(',');
+					if (find <= 0)
+					{
+						continue;
+					}
+					buf2 = buf.Substring(0, find);
+					if (int.TryParse(buf2, out motor_R) == false)
+					{
+						continue;
+					}
+					if (cmd == 3)
+					{
+						label1.Invoke(new MyDelegate(SetText), new object[] { Convert.ToString(motor_R) });
+					}
+					//moter_Lの値取得
+					buf = buf.Remove(0, find + 1);
+					buf2 = buf;
+					if (int.TryParse(buf2, out motor_L) == false)
+					{
+						continue;
+					}
+					if (cmd == 4)
+					{
+						label1.Invoke(new MyDelegate(SetText), new object[] { Convert.ToString(motor_L) });
+					}
+				}
+			}
+			else
+			{
+				MessageBox.Show("error", "error");
+			}
+		}
+		void SetText(string target)
+		{
+			label1.Text = target;
+		}
+	}
+
+}
